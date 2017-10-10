6919f8579f29e1d0a276dff53aa624a3dfd04be1
Zach Dicklin <zdicklin@gmail.com>
2017-10-09 18:14:20 -0700

    Remove conflicting CSS

--- front/app/App.vue
+++ front/app/App.vue
@@ -22,11 +22,3 @@
   }
 
 </script>
-
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-</style>
