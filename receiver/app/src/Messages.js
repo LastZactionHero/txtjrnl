b42748cfaf50f22ac971f50f37f5d6303333b37e
Zach Dicklin <zdicklin@gmail.com>
2017-10-24 20:44:39 -0600

    Update message prompts

--- receiver/app/src/Messages.js
+++ receiver/app/src/Messages.js
@@ -3,7 +3,7 @@ export default {
     return [
       "Welcome to txtjrnl.",
       "We’re a tool to help you capture your day, your thoughts and your feelings in an easy, familiar way.",
-      "Any txt (including photos, links and media) sent to this number will be captured in your online jrnl, which you can access at txtjrnl.com.",
+      "Any txt, including photos, links, GIFs and iMessage app content, sent to this number will be captured in your online jrnl, which you can access at txtjrnl.com.",
       "We suggest adding this number to your contacts, and even giving it a name in your contacts.",
       "Start journaling!"
     ]
@@ -12,9 +12,9 @@ export default {
   inactive() {
     return [
       { time: { hour: 8, minute: 45 }, message: 'What would you like to accomplish today?', firstMessageOfTheDay: true },
-      { time: { hour: 12, minute: 0 }, message: 'How are things going today?' },
-      { time: { hour: 18, minute: 0 }, message: 'How did today go?' },
-      { time: { hour: 21, minute: 0 }, message: 'Any thoughts as you finish your day?', lastMessageOfTheDay: true }
+      { time: { hour: 12, minute: 0 }, message: 'How’s it going so far?' },
+      { time: { hour: 18, minute: 0 }, message: 'How did today turn out for you? What would you like to capture in your jrnl?' },
+      { time: { hour: 21, minute: 0 }, message: 'Any final thoughts as you finish your day?', lastMessageOfTheDay: true }
     ]
   },
 
