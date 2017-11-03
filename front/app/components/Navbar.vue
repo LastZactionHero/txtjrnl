288bb60b5caae79e118ca06a2836f8e84db02f3c
Zach Dicklin <zdicklin@gmail.com>
2017-11-02 20:56:43 -0600

    Bugfix: Navbar logo link

--- front/app/components/Navbar.vue
+++ front/app/components/Navbar.vue
@@ -9,7 +9,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">TxtJrnl</a>
+        <router-link to="/" class='navbar-brand'>TxtJrnl</router-link>
       </div>
 
       <!-- Collect the nav links, forms, and other content for toggling -->
